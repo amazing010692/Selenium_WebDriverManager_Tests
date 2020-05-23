1250a3018c739c0e60a61e851546a5377c17011c
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-05-23 16:18:13 +0800

    Synchronization - Implicit Wait

--- src/test/java/testcases/TestFindingElements.java
+++ src/test/java/testcases/TestFindingElements.java
@@ -1,5 +1,7 @@
 package testcases;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -15,13 +17,17 @@ public class TestFindingElements {
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://gmail.com");
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 		WebElement username = driver.findElement(By.id("identifierId"));
-		username.sendKeys("tcoptestcebumabolo@gmail.com");
+		username.sendKeys("java@way2automation.com");
 		
 		//driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/span/span")).click();
 		WebElement nextButton = driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/span/span"));
 		nextButton.click();
+
+		WebElement password = driver.findElement(By.name("password"));
+		password.sendKeys("TestPassword123");
 		
 		Thread.sleep(3000);
 		driver.close();
