9b08c325aac56e2b913973c724b0e7467a21f288
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-06-09 20:49:08 +0800

    Mouse hover with explicit wait

--- src/test/java/testcases/MouseHover.java
+++ src/test/java/testcases/MouseHover.java
@@ -13,23 +13,15 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.opera.OperaOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class MouseHover {
 	
 	public static String browser = "chrome"; //excel sheet
 	public static WebDriver driver;
-	
-	public static boolean isElementPresent(By by) {
-
-		int size = driver.findElements(by).size();
-		if(size == 0) {
-			return false;			
-		} else {
-			return true;
-		}
-
-	}
 
 	public static void main(String[] args) throws InterruptedException {
 		
@@ -63,6 +55,7 @@ public class MouseHover {
 		//Pre-conditions | Maximize the browser and apply implicit waits.
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebDriverWait wait = new WebDriverWait(driver, 20);
 		
 		//Navigate to this site for sample checkbox.
 		driver.get(" https://americangolf.co.uk");
@@ -72,16 +65,29 @@ public class MouseHover {
 		Actions action = new Actions(driver);
 		WebElement GolfClubs = driver.findElement(By.xpath("//*[@id='header-navigation']/div[1]/ul/li[2]/a"));
 		action.moveToElement(GolfClubs).build().perform();
+		System.out.println("Hover mouse to Golf Clubs.");
 		
 		//Click Drivers link under Golf Clubs.
 		WebElement linkDrivers = driver.findElement(By.xpath("//div[@id='CLUBS_1']//span[@class='name'][contains(text(),'Drivers')]"));
 		linkDrivers.click();
+		System.out.println("Drivers has been clicked.");
 		
 		//Hover the mouse to Clothings & Shoes
-		WebElement ClothingsShoes = driver.findElement(By.xpath("//a[@class='a-level-1'][contains(text(),'Clothing & Shoes')]"));
-		action.moveToElement(ClothingsShoes).build().perform();
+		String xpathClothingsShoes = "//a[@class='a-level-1'][contains(text(),'Clothing & Shoes')]";
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpathClothingsShoes)));
+		WebElement clothingsShoes = driver.findElement(By.xpath(xpathClothingsShoes));
+		action.moveToElement(clothingsShoes).build().perform();
+		System.out.println("Hover mouse to Clothings & Shoes.");
 		
 		//Click Shirts link under Clothings & Shoes
+		String xpathShirts = "//div[@id='CLOTHFOOTW_1']//span[@class='name'][contains(text(),'Shirts')]";
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpathShirts)));
+		WebElement shirts = driver.findElement(By.xpath(xpathShirts));
+		shirts.click();
+		System.out.println("Shirts has been clicked.");
+		
+		//Quit the WebDriver session.
+		driver.quit();
 	}
 
 }
