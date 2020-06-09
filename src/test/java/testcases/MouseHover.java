f64ea74079ec00086081ba7bb17bed55e7df8e3a
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-06-09 20:24:59 +0800

    2nd commit - mouse hover

--- src/test/java/testcases/MouseHover.java
+++ src/test/java/testcases/MouseHover.java
@@ -2,11 +2,14 @@ package testcases;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.opera.OperaOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -16,6 +19,17 @@ public class MouseHover {
 	
 	public static String browser = "chrome"; //excel sheet
 	public static WebDriver driver;
+	
+	public static boolean isElementPresent(By by) {
+
+		int size = driver.findElements(by).size();
+		if(size == 0) {
+			return false;			
+		} else {
+			return true;
+		}
+
+	}
 
 	public static void main(String[] args) throws InterruptedException {
 		
@@ -53,7 +67,21 @@ public class MouseHover {
 		//Navigate to this site for sample checkbox.
 		driver.get(" https://americangolf.co.uk");
 		System.out.println("TITLE: " + driver.getTitle() + " | URL: " + driver.getCurrentUrl());
-
+		
+		//Use Actions class to hover the mouse to Golf Clubs.
+		Actions action = new Actions(driver);
+		WebElement GolfClubs = driver.findElement(By.xpath("//*[@id='header-navigation']/div[1]/ul/li[2]/a"));
+		action.moveToElement(GolfClubs).build().perform();
+		
+		//Click Drivers link under Golf Clubs.
+		WebElement linkDrivers = driver.findElement(By.xpath("//div[@id='CLUBS_1']//span[@class='name'][contains(text(),'Drivers')]"));
+		linkDrivers.click();
+		
+		//Hover the mouse to Clothings & Shoes
+		WebElement ClothingsShoes = driver.findElement(By.xpath("//a[@class='a-level-1'][contains(text(),'Clothing & Shoes')]"));
+		action.moveToElement(ClothingsShoes).build().perform();
+		
+		//Click Shirts link under Clothings & Shoes
 	}
 
 }
