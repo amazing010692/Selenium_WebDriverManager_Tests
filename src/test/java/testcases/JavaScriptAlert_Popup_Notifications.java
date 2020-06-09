b9421e814eb0279c522f3d4a6dfb043c58e416a1
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-06-09 13:50:31 +0800

    Rename class and update with popup and notifications

--- src/test/java/testcases/JavaScriptAlert_Popup_Notifications.java
+++ src/test/java/testcases/JavaScriptAlert_Popup_Notifications.java
@@ -14,7 +14,7 @@ import org.openqa.selenium.opera.OperaOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class JavaScript_Alert {
+public class JavaScriptAlert_Popup_Notifications {
 	
 	public static String browser = "chrome"; //excel sheet
 	public static WebDriver driver;
