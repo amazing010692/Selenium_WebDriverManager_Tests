cdb2d7145fcba6a15f8282d1708acf876febd18b
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-06-13 17:15:04 +0800

    4th commit - JavaScript Executor

--- src/test/java/testcases/JavaScript_Executor.java
+++ src/test/java/testcases/JavaScript_Executor.java
@@ -64,8 +64,8 @@ public class JavaScript_Executor {
 		fieldLeavingFrom.sendKeys("BENG");
 		
 		//Press the Down key twice and print the attribute value.
-		Thread.sleep(1000);
 		fieldLeavingFrom.sendKeys(Keys.DOWN);
+		Thread.sleep(2000);
 		fieldLeavingFrom.sendKeys(Keys.DOWN);
 		
 		//Create an object of JavascriptExecutor interface by Type casting.
@@ -78,6 +78,45 @@ public class JavaScript_Executor {
 		String text = (String) js.executeScript(script);
 		System.out.println(text);
 		
+		//Navigate to this site for sample javascript executor.
+		driver.get("https://ksrtc.in/oprs-web/");
+		System.out.println("TITLE: " + driver.getTitle() + " | URL: " + driver.getCurrentUrl());
+		
+		//Type "BENG" in the Leaving From field.
+		WebElement fieldLeavingFrom1 = driver.findElement(By.xpath("//input[@id='fromPlaceName']"));
+		fieldLeavingFrom1.sendKeys("BENG");
+		
+		//Press the Down key twice and print the attribute value.
+		fieldLeavingFrom1.sendKeys(Keys.DOWN);
+		Thread.sleep(2000);
+		fieldLeavingFrom1.sendKeys(Keys.DOWN);
+		
+		//Create an object of JavascriptExecutor interface by Type casting.
+		JavascriptExecutor js1 = (JavascriptExecutor)driver;
+		
+		//Add the return keyword and store it in a variable.
+		String script1 = "return document.getElementById(\"fromPlaceName\").value;";
+		
+		//Execute script and print the text in console.
+		String text1 = (String) js1.executeScript(script1);
+		System.out.println(text1);
+		
+		/*Keeps scrolling down until "BENGALURU INTERNATION AIRPORT" gets selected. 
+		Script tries for 10 runs, than prints ‘Element not found’*/
+		int i = 0;
+		while(!text1.equalsIgnoreCase("bengaluru inernation airport")) {
+			i++;
+			fieldLeavingFrom1.sendKeys(Keys.DOWN);
+			Thread.sleep(1000);
+			text1 = (String) js.executeScript(script1);
+			System.out.println(text1);
+			if(i > 10)
+				break;
+		}
+		if(i > 10) {
+			System.out.println("Element not found");
+		} else
+			System.out.println("Element found");
 	}
 
 }
