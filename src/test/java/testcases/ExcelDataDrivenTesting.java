5c94ea1338320928bbe56d088c79ae876b574593
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-06-16 18:12:17 +0800

    5th commit - Excel Data Driven Testing

--- src/test/java/testcases/ExcelDataDrivenTesting.java
+++ src/test/java/testcases/ExcelDataDrivenTesting.java
@@ -1,61 +1,23 @@
 package testcases;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
+import java.util.Iterator;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.opera.OperaDriver;
-import org.openqa.selenium.opera.OperaOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ExcelDataDrivenTesting {
-	
-	public static String browser = "chrome"; //excel sheet
-	public static WebDriver driver;
 
 	public static void main(String[] args) throws InterruptedException, IOException {
-		
-		if(browser.equals("chrome")) {
-			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
-			
-		} else if(browser.equals("firefox")) {
-			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
-			
-		} else if(browser.equals("ie")) {			
-			WebDriverManager.iedriver().setup();
-			driver = new InternetExplorerDriver();
-			
-		} else if(browser.equals("edge")) {			
-			WebDriverManager.edgedriver().setup();
-			driver = new EdgeDriver();
-		
-		} else if(browser.equals("opera")) {			
-			DesiredCapabilities capabilities = new DesiredCapabilities();
-			OperaOptions options = new OperaOptions();
-			options.setBinary("C:\\Users\\hello\\AppData\\Local\\Programs\\Opera\\64.0.3417.73\\opera.exe");
-			capabilities.setCapability(OperaOptions.CAPABILITY, options);
-			
-			WebDriverManager.operadriver().setup();
-			driver = new OperaDriver(options);
-		
-		}
 		
 		//Create FileInputStream object pointing towards the excel (make sure that the filepath is within double quotes
 		FileInputStream fis = new FileInputStream("C:\\Users\\hello\\Documents\\TestDataExcel.xlsx");
 		
 		//Create an object of XSSFWorkbook class to take control of the entire excel application.
+		@SuppressWarnings("resource")
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);	
 		
 		//Below logic to fetch “TestData” sheet from "TestDataExcel" file.
@@ -63,6 +25,26 @@ public class ExcelDataDrivenTesting {
 		for(int i = 0; i < sheets; i++) {
 			if(workbook.getSheetName(i).equalsIgnoreCase("TestData")) {
 				XSSFSheet sheet = workbook.getSheetAt(i);				//Grab "TestData" sheet
+				Iterator<Row> rows = sheet.iterator();					//Iterate through each rows
+				Row firstrow = rows.next();								//We are on the first row now.
+				Iterator<Cell> ce = firstrow.cellIterator();			//Iterate through each cell of the first row.
+				/*Next, we will define 2 variables: ‘k’ and ‘column’. The ‘column’ variable will 
+				 store the column index (starts from 0). The variable ‘k’ will keep getting incremented with each ‘while’ loop. */	 
+				int k = 0;
+				int column = 0;
+				/*Create a ‘while’ loop that will loop till the time a cell is present. 
+				If the value of first cell is equal to “Testcases”, we will grab the column*/
+				while(ce.hasNext()) {			//Looping until the time cell is present.
+					Cell value = ce.next();		//In 1st loop, we are in the 1st cell of 1st row.
+												//In 2nd loop (if any) we will move to the 2nd cell of 1st row.
+					if(value.getStringCellValue().equalsIgnoreCase("Testcases")) {
+						//grab desired column
+						column = k;
+					}
+					k++;
+				}
+				//Print the value of ‘column’ variable
+				System.out.println(column);
 			}
 		}
 		
