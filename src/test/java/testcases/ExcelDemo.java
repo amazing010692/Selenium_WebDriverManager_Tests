efadf7360421dbdb544d84a63ac3b98f9d9e52b3
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-06-17 16:29:35 +0800

    11th commit - Excel Data Driven Testing

--- src/test/java/testcases/ExcelDemo.java
+++ src/test/java/testcases/ExcelDemo.java
@@ -7,6 +7,8 @@ import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -61,7 +63,14 @@ public class ExcelDemo {
 						Iterator<Cell> cv = r.cellIterator(); //Iterate through each cell of TC3 row.
 						while(cv.hasNext()) {
 							//System.out.println(cv.next().getStringCellValue());
-							a.add(cv.next().getStringCellValue());
+							//a.add(cv.next().getStringCellValue());
+							
+							Cell c = cv.next();
+							if(c.getCellType()==XSSFCell.CELL_TYPE_STRING) {
+								a.add(c.getStringCellValue());
+							} else {
+								a.add(NumberToTextConverter.toText(c.getNumericCellValue()));
+							}
 						}
 					}
 				}	
