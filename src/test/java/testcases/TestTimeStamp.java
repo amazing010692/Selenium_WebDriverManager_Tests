f11ee0ffdfe8ae7ddf4ca7967c1e09ca5fa59871
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-06-24 20:51:08 +0800

    9th commit - Capture Screenshot with TimeStamp

--- src/test/java/testcases/TestTimeStamp.java
+++ src/test/java/testcases/TestTimeStamp.java
@@ -1,5 +1,7 @@
 package testcases;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class TestTimeStamp {
@@ -9,7 +11,12 @@ public class TestTimeStamp {
 		//Prints the current system date and convert it to string.
 		Date d = new Date();
 		System.out.println(d.toString().replace(":", "_").replace(" ", "_") + ".jpg");
-
+		
+		//Get Current Date and Time: java.time.format.DateTimeFormatter
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+		LocalDateTime now = LocalDateTime.now();  
+		System.out.println(dtf.format(now));  
+		
 	}
 
 }
