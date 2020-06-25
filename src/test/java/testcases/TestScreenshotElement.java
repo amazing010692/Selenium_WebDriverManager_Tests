5908f496d8b40e5e5406fb32747fab52d53038bb
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-06-25 12:26:03 +0800

    1st commit - Capturing Particular Element Screenshot

---
+++ src/test/java/testcases/TestScreenshotElement.java
@@ -0,0 +1,97 @@
+package testcases;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestScreenshotElement {
+	
+	//Utility for capturing screenshot.
+	public static void captureScreenshot() throws IOException {
+		Date d = new Date();
+		String fileName = d.toString().replace(":", "_").replace(" ", "_") + ".jpg";
+		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(screenshot, new File(".//screenshot//" + fileName));
+	}
+	
+	public static String browser = "chrome"; //excel sheet
+	public static WebDriver driver;
+	
+	public static void main(String[] args) throws InterruptedException, IOException {
+		
+		if(browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			
+		} else if(browser.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			
+		} else if(browser.equals("ie")) {			
+			WebDriverManager.iedriver().setup();
+			driver = new InternetExplorerDriver();
+			
+		} else if(browser.equals("edge")) {			
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		
+		} else if(browser.equals("opera")) {			
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			OperaOptions options = new OperaOptions();
+			options.setBinary("C:\\Users\\hello\\AppData\\Local\\Programs\\Opera\\64.0.3417.73\\opera.exe");
+			capabilities.setCapability(OperaOptions.CAPABILITY, options);
+			
+			WebDriverManager.operadriver().setup();
+			driver = new OperaDriver(options);
+		
+		}
+		
+		//Pre-conditions | Maximize the browser and apply implicit waits.
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			
+		//Navigate to this site for sample JavaScript Executor using typecasting.
+		driver.get("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_submit_get");
+		System.out.println("TITLE: " + driver.getTitle() + " | URL: " + driver.getCurrentUrl());
+		
+		//Switch to frame by id name.
+		driver.switchTo().frame("iframeResult");
+		
+		//Click the "Try It" button using Javascript executor with typecasting.
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("myFunction()");
+		System.out.println("Try It button has been successfully clicked.");
+		
+		//Highlight the "Submit the Form" button with Javascript Executor.
+		WebElement buttonSubmitForm = driver.findElement(By.xpath("//*[@id=\"mySubmit\"]"));
+		js.executeScript("arguments[0].style.border='3px solid red'", buttonSubmitForm);
+		System.out.println("Submit the Form button has been successfully highlighted.");
+		
+		//Capture screenshot and store in a current directory.
+		captureScreenshot();
+		System.out.println("Screenshot is generated in the destination path.");
+		
+		//Quits the WebDriver session.
+		driver.quit();
+
+	}
+
+}
